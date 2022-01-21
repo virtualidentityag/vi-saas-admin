836dc3b9c01efea2e71f5a2e14772f7ff313a20c
Timo Koenig <koenig@kaiserkoenig.de>
2022-01-21 17:29:36 +0100

    added test, TenantList + Layout improvements

---
+++ src/components/TenantList/TenantsList.tsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import { Table, message } from "antd";
+import moment from "moment";
+import getCancelTokenSource from "../../api/getCancelTokenSource";
+import getFAKETenantData from "../../api/tenant/getFAKETenantData";
+import { TenantData } from "../../types/tenant";
+
+function TenantsList() {
+  const [tenants, setTenants] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const cancelTokenSource = getCancelTokenSource();
+    getFAKETenantData(cancelTokenSource)
+      .then((result: any) => {
+        setIsLoading(false);
+        setTenants(result);
+
+        console.log(result);
+      })
+      .catch(() => {
+        setIsLoading(false);
+        message.error({
+          content:
+            "Es ist ein Fehler aufgetreten. Bitte versuchen Sie es später noch einmal",
+          duration: 3,
+        });
+      });
+
+    return () => {
+      cancelTokenSource.cancel();
+    };
+  }, []);
+
+  const columns: any[] = [
+    {
+      title: "Organisation",
+      dataIndex: "name",
+      key: "name",
+      sorter: (a: TenantData, b: TenantData) => a.name.localeCompare(b.name),
+      width: 150,
+      ellipsis: true,
+      fixed: "left",
+    },
+    {
+      width: 250,
+      title: "Subdomain",
+      dataIndex: "subdomain",
+      ellipsis: true,
+      key: "subdomain",
+      sorter: (a: TenantData, b: TenantData) =>
+        a.subdomain.localeCompare(b.subdomain),
+    },
+    {
+      title: "Hinzugefügt am",
+      dataIndex: "createDate",
+      key: "createDate",
+      ellipsis: true,
+      width: 150,
+      sorter: (a: TenantData, b: TenantData) =>
+        moment(a.createDate).unix() - moment(b.createDate).unix(),
+      render: (date: Date) => {
+        return <span> {date.toLocaleDateString("de-DE")}</span>;
+      },
+    },
+    {
+      title: "Anzahl von Benutzern",
+      width: 150,
+      ellipsis: true,
+      render: (record: { licensing: { allowedNumberOfUsers: number } }) =>
+        record.licensing.allowedNumberOfUsers,
+      sorter: (a: TenantData, b: TenantData) =>
+        a.licensing.allowedNumberOfUsers - b.licensing.allowedNumberOfUsers,
+    },
+  ];
+
+  return (
+    <Table
+      loading={isLoading}
+      className="tenantsTable"
+      dataSource={tenants}
+      columns={columns}
+      scroll={{
+        x: "max-content",
+        y: "100%",
+      }}
+      sticky
+      tableLayout="fixed"
+    />
+  );
+}
+
+export default TenantsList;

5b4eb237a29081c70f0e5b0d52d69e05c8392a2e
Timo Koenig <koenig@kaiserkoenig.de>
2022-03-14 10:08:43 +0100

    fix: API using env vars

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,21 +1,17 @@
 import getLocationVariables from "./utils/getLocationVariables";
 
-process.env.REACT_APP_API_URL =
-  process.env.REACT_APP_API_URL_FOR_LOCAL_DEVELOPMENT;
 const REACT_APP_CSRF_WHITELIST_HEADER_PROPERTY =
   process.env.REACT_APP_CSRF_WHITELIST_HEADER_FOR_LOCAL_DEVELOPMENT;
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 export const CSRF_WHITELIST_HEADER: string =
   REACT_APP_CSRF_WHITELIST_HEADER_PROPERTY;
-export const apiUrlEnv: string = process.env.REACT_APP_API_URL || "";
-export const apiUrl = process.env.REACT_APP_API_URL
-  ? `https://${apiUrlEnv}`
-  : "";
-
 const { subdomain } = getLocationVariables();
 
-export const mainURL = `https://${subdomain}.develop.onlineberatung.net`;
+export const mainURL = subdomain
+  ? `https://${subdomain}.${process.env.REACT_APP_API_URL}`
+  : `https://${process.env.REACT_APP_API_URL}`;
+
 export const XHRheader = { AcceptLanguage: "de" };
 export const loginEndpoint = `${mainURL}/auth/realms/online-beratung/protocol/openid-connect/token`;
 export const logoutEndpoint = `${mainURL}/auth/realms/online-beratung/protocol/openid-connect/logout`;

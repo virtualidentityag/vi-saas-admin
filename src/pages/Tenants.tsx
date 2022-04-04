ba1a4bd95d544ee3ed9a51bd392af4ec98d8762a
Timo Koenig <koenig@kaiserkoenig.de>
2022-04-04 07:32:29 +0200

    feat: redirect to settings page if not a superadmin

--- src/pages/Tenants.tsx
+++ src/pages/Tenants.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import TenantsList from "../components/TenantList/TenantsList";
+import routePathNames from "../appConfig";
 
 function Tenants() {
-  return (
+  const { isSuperAdmin } = useSelector((state: any) => state.tenantData);
+
+  return !isSuperAdmin ? (
+    <Navigate to={routePathNames.themeSettings} />
+  ) : (
     <div>
       <TenantsList />
     </div>

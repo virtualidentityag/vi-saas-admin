cec55c4bd62c82c473c29f7d93c3aa72f604da5d
aalicic <adnan@leads.ba>
2022-04-12 15:28:33 +0200

    bugfix/VIC-580-Several_agencies_per_tenant

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
     ) {
       navigate("/admin/theme-settings");
     }
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return renderAppComponent ? (
     <ProtectedPageLayoutWrapper>

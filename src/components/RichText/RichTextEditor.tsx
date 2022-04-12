3514c175a0fe16451b0991322e871fdb74274778
Sergio F <sfvsound@gmail.com>
2022-04-12 09:32:41 +0200

    fix: translation import fixed

--- src/components/RichText/RichTextEditor.tsx
+++ src/components/RichText/RichTextEditor.tsx
@@ -1,32 +1,6 @@
 import React, { useState } from "react";
 import RichTextEditor, { EditorValue, ToolbarConfig } from "react-rte";
-
-const toolbarConfig: ToolbarConfig = {
-  // Optionally specify the groups to display (displayed in the order listed).
-  display: [
-    "BLOCK_TYPE_DROPDOWN",
-    "INLINE_STYLE_BUTTONS",
-    "BLOCK_TYPE_BUTTONS",
-    "LINK_BUTTONS",
-    "IMAGE_BUTTON",
-  ],
-  INLINE_STYLE_BUTTONS: [
-    { label: "Bold", style: "BOLD", className: "custom-css-class" },
-    { label: "Italic", style: "ITALIC" },
-    { label: "Underline", style: "UNDERLINE" },
-  ],
-  BLOCK_TYPE_DROPDOWN: [
-    { label: "Text", style: "unstyled" },
-    { label: "Headline 1. Ordnung", style: "header-one" },
-    { label: "Headline 2. Ordnung", style: "header-two" },
-    { label: "Headline 3. Ordnung", style: "header-three" },
-    { label: "Headline 4. Ordnung", style: "header-four" },
-  ],
-  BLOCK_TYPE_BUTTONS: [
-    { label: "UL", style: "unordered-list-item" },
-    { label: "OL", style: "ordered-list-item" },
-  ],
-};
+import { useTranslation } from "react-i18next";
 
 interface OnChangeHandler {
   (e: any): void;
@@ -39,6 +13,7 @@ type RichTextEditorProps = {
 };
 
 function RTE({ value, onChange, placeholder }: RichTextEditorProps) {
+  const { t } = useTranslation();
   const [editorState, setEditorState] = useState(() =>
     RichTextEditor.createValueFromString(value, "html")
   );
@@ -46,6 +21,33 @@ function RTE({ value, onChange, placeholder }: RichTextEditorProps) {
   const handleChange = (edited: EditorValue) => {
     setEditorState(edited);
     onChange(edited);
+  };
+
+  const toolbarConfig: ToolbarConfig = {
+    // Optionally specify the groups to display (displayed in the order listed).
+    display: [
+      "BLOCK_TYPE_DROPDOWN",
+      "INLINE_STYLE_BUTTONS",
+      "BLOCK_TYPE_BUTTONS",
+      "LINK_BUTTONS",
+      "IMAGE_BUTTON",
+    ],
+    INLINE_STYLE_BUTTONS: [
+      { label: "Bold", style: "BOLD", className: "custom-css-class" },
+      { label: "Italic", style: "ITALIC" },
+      { label: "Underline", style: "UNDERLINE" },
+    ],
+    BLOCK_TYPE_DROPDOWN: [
+      { label: t("rte.text"), style: "unstyled" },
+      { label: t("rte.h1"), style: "header-one" },
+      { label: t("rte.h2"), style: "header-two" },
+      { label: t("rte.h3"), style: "header-three" },
+      { label: t("rte.h4"), style: "header-four" },
+    ],
+    BLOCK_TYPE_BUTTONS: [
+      { label: "UL", style: "unordered-list-item" },
+      { label: "OL", style: "ordered-list-item" },
+    ],
   };
 
   return (

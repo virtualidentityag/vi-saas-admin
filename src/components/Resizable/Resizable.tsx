59eee8bec13a363f4a903f98cf33d6077a89c266
sergiofv <sfvsound@gmail.com>
2022-06-22 10:48:32 +0200

    fix: left margin removed when resizing & removed sticky for counselors

--- src/components/Resizable/Resizable.tsx
+++ src/components/Resizable/Resizable.tsx
@@ -30,7 +30,7 @@ function ResizableTitle(props: any) {
       draggableOpts={{
         enableUserSelectHack: false,
       }}
-      maxConstraints={[700, 700]}
+      maxConstraints={[500, 500]}
       minConstraints={[50, 50]}
     >
       <th {...restProps} />

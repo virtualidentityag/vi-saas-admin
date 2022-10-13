93fd80cf79db16c3101a0bab98777906400c25f9
Carlos Soares <carlos.soares17@gmail.com>
2022-10-13 15:32:12 +0100

    fix: empty items in berater when selecting agencies

--- src/utils/convertToOptions.ts
+++ src/utils/convertToOptions.ts
@@ -3,17 +3,25 @@ import { Option } from "../components/SelectFormField";
 export const convertToOptions = <T>(
   data: T[],
   labelKey: keyof T | Array<keyof T>,
-  valueKey: keyof T
+  valueKey: keyof T,
+  filterEmptyValues = false
 ): Option[] => {
+  const finalData = filterEmptyValues
+    ? data?.filter(
+        (tmpData) =>
+          tmpData[valueKey] !== null && tmpData[valueKey] !== undefined
+      )
+    : data;
+
   return (
-    data?.map((d) => {
+    finalData?.map((d) => {
       const label =
         labelKey instanceof Array
           ? labelKey.map((key) => d[key]).join(" ")
           : d[labelKey];
       return {
         label,
-        value: d[valueKey].toString() as unknown,
+        value: d[valueKey]?.toString() as unknown,
       } as Option;
     }) || []
   );

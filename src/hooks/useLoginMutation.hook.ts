e00daf80d30ccaff910a7f53d0d939975d11e2f0
Carlos Soares <carlos.soares17@gmail.com>
2022-10-31 09:41:01 +0000

    fix: login when we've the multitenancy with single domain enabled

--- src/hooks/useLoginMutation.hook.ts
+++ src/hooks/useLoginMutation.hook.ts
@@ -2,6 +2,7 @@ import { useMutation } from "react-query";
 import { setTokens } from "../api/auth/auth";
 import getAccessToken from "../api/auth/getAccessToken";
 import { FETCH_ERRORS } from "../api/fetchData";
+import { useAppConfigContext } from "../context/useAppConfig";
 import { TwoFactorType } from "../enums/TwoFactorType";
 import { LoginData } from "../types/loginData";
 import parseJwt from "../utils/parseJWT";
@@ -20,10 +21,15 @@ interface ErrorLogin {
 }
 
 export const useLoginMutation = (tenantId: string) => {
+  const { settings } = useAppConfigContext();
   return useMutation<LoginData, ErrorLogin, LoginParams>(
     ["login", "user-data", tenantId],
     async ({ username, password, otp }: any) => {
       return getAccessToken({ username, password, otp }).then((data) => {
+        if (settings?.multitenancyWithSingleDomainEnabled) {
+          return data;
+        }
+
         const { tenantId: userTenantId } = parseJwt(data.access_token);
         return userTenantId === tenantId
           ? data

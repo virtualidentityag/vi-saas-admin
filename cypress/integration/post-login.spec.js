64abb57f0c997871a19958d9f00592227cb2a913
Carlos Soares <carlos.soares17@gmail.com>
2022-11-22 15:58:09 +0000

    feat: finish prittier of all files

--- cypress/integration/post-login.spec.js
+++ cypress/integration/post-login.spec.js
@@ -1,28 +1,28 @@
 /* eslint-disable cypress/no-unnecessary-waiting */
 /* eslint-disable func-names */
 
-const USERONEEMAIL = Cypress.env("userOne");
-const USERONEPASSWORD = Cypress.env("passwordOne");
+const USERONEEMAIL = Cypress.env('userOne');
+const USERONEPASSWORD = Cypress.env('passwordOne');
 
-describe("Basic Test", function () {
-  it("Checks the Title", function () {
-    cy.visit("/");
-    /* This tests need to be updated to the new admin version
+describe('Basic Test', function () {
+    it('Checks the Title', function () {
+        cy.visit('/');
+        /* This tests need to be updated to the new admin version
     cy.contains("Admin Login");
     */
-  });
+    });
 
-  it("Checks the URL", function () {
-    cy.url().should("include", "localhost:3000/admin");
-  });
+    it('Checks the URL', function () {
+        cy.url().should('include', 'localhost:3000/admin');
+    });
 });
 
-describe("URL-Test Settings ", function () {
-  afterEach(() => {
-    cy.get("span").contains("Logout").click();
-  });
+describe('URL-Test Settings ', function () {
+    afterEach(() => {
+        cy.get('span').contains('Logout').click();
+    });
 
-  /* This tests need to be updated to the new admin version
+    /* This tests need to be updated to the new admin version
   it("Checks the URL in Settings", function () {
     cy.visit("/");
     cy.get("#basic_username").type(USERONEEMAIL);
@@ -34,12 +34,12 @@ describe("URL-Test Settings ", function () {
   */
 });
 
-describe("URL-Tests Counselor", function () {
-  afterEach(() => {
-    cy.get("span").contains("Logout").click();
-  });
+describe('URL-Tests Counselor', function () {
+    afterEach(() => {
+        cy.get('span').contains('Logout').click();
+    });
 
-  /* This tests need to be updated to the new admin version
+    /* This tests need to be updated to the new admin version
   it("Checks the URL in berater", function () {
     cy.visit("/");
     cy.get("#basic_username").type(USERONEEMAIL);
@@ -51,8 +51,8 @@ describe("URL-Tests Counselor", function () {
   */
 });
 
-describe("Check lateral navigtion", function () {
-  /* This tests need to be updated to the new admin version
+describe('Check lateral navigtion', function () {
+    /* This tests need to be updated to the new admin version
   beforeEach(() => {
     cy.visit("/");
     cy.get("#basic_username").type(USERONEEMAIL);
@@ -60,33 +60,33 @@ describe("Check lateral navigtion", function () {
     cy.get(".ant-btn-primary").click();
   });
   */
-  /* This tests need to be updated to the new admin version
+    /* This tests need to be updated to the new admin version
    it("Checks the URL in Dashboard Button", function () {
     cy.get(".menuItem").eq(0).click();
     cy.url().should("include", `localhost:3000`);
   });
   */
-  /* This tests need to be updated to the new admin version
+    /* This tests need to be updated to the new admin version
   it("Checks the URL in Settings Button", function () {
     cy.get("span").contains("Einstellungen").click();
     cy.url().should("include", "/theme-settings");
     cy.get("span").contains("Logout").click();
   });
   */
-  /* This tests need to be updated to the new admin version
+    /* This tests need to be updated to the new admin version
   it("Checks the URL in Berater Button", function () {
     cy.get("span").contains("Berater").click();
     cy.url().should("include", "/berater");
     cy.get("span").contains("Logout").click();
   });
   */
-  /* This tests need to be updated to the new admin version
+    /* This tests need to be updated to the new admin version
   it("Checks the URL in Profil Button", function () {
     cy.get(".menuItem").eq(3).click();
     cy.url().should("include", "/profil");
   });
   */
-  /* This tests need to be updated to the new admin version
+    /* This tests need to be updated to the new admin version
   it("Checks the URL in Logout Button", function () {
     cy.get("span").contains("Logout").click();
     cy.url().should("include", "/login");

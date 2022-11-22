cf7c4aa9e4691bde63b6d2574cfc4ad6207d2f22
Carlos Soares <carlos.soares17@gmail.com>
2022-11-22 15:56:03 +0000

    feat: update the hooks

--- src/hooks/useTenantDataMutation.hook.ts
+++ src/hooks/useTenantDataMutation.hook.ts
@@ -1,7 +1,7 @@
-import { useMutation } from "react-query";
-import editTenantData from "../api/tenant/editTenantData";
-import { TenantData } from "../types/tenant";
+import { useMutation } from 'react-query';
+import editTenantData from '../api/tenant/editTenantData';
+import { TenantData } from '../types/tenant';
 
 export const useTenantDataMutation = () => {
-  return useMutation("tenant-data", (data: TenantData) => editTenantData(data));
+    return useMutation('tenant-data', (data: TenantData) => editTenantData(data));
 };
--- src/hooks/useUserRoles.hook.ts
+++ src/hooks/useUserRoles.hook.ts
@@ -1,17 +1,14 @@
-import { getValueFromCookie } from "../api/auth/accessSessionCookie";
-import { UserRole } from "../enums/UserRole";
-import parseJwt from "../utils/parseJWT";
+import { getValueFromCookie } from '../api/auth/accessSessionCookie';
+import { UserRole } from '../enums/UserRole';
+import parseJwt from '../utils/parseJWT';
 
 export const useUserRoles = (): [UserRole[], (role: UserRole) => boolean] => {
-  const accessToken = getValueFromCookie("keycloak");
-  let roles: UserRole[] = [];
-  if (accessToken) {
-    const access = parseJwt(accessToken || "");
-    roles = access?.realm_access.roles || [];
-  }
+    const accessToken = getValueFromCookie('keycloak');
+    let roles: UserRole[] = [];
+    if (accessToken) {
+        const access = parseJwt(accessToken || '');
+        roles = access?.realm_access.roles || [];
+    }
 
-  return [
-    roles,
-    (userRole: UserRole) => roles.some((role: UserRole) => role === userRole),
-  ];
+    return [roles, (userRole: UserRole) => roles.some((role: UserRole) => role === userRole)];
 };

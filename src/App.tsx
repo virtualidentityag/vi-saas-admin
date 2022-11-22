9aa5772b48185c89d92af224215810947651d07f
Carlos Soares <carlos.soares17@gmail.com>
2022-11-22 15:57:55 +0000

    feat: update the rest of the ts files

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,61 @@
-import { useEffect } from "react";
-import "./styles/App.less";
-import { Route, Routes, useLocation, useNavigate } from "react-router";
-import ProtectedPageLayoutWrapper from "./components/Layout/ProtectedPageLayoutWrapper";
-import routePathNames from "./appConfig";
+import { useEffect } from 'react';
+import './styles/App.less';
+import { Route, Routes, useLocation, useNavigate } from 'react-router';
+import ProtectedPageLayoutWrapper from './components/Layout/ProtectedPageLayoutWrapper';
+import routePathNames from './appConfig';
 // import Dashboard from "./pages/Dashboard";
-import TenantSettings from "./pages/TenantSettings";
-import Counselors from "./pages/Counselors";
-import Topics from "./pages/Topics";
-import Statistic from "./pages/Statistic";
-import UserProfile from "./pages/UserProfile";
-import Tenants from "./pages/Tenants";
-import Initialization from "./components/Layout/Initialization";
-import Agencies from "./pages/Agencies";
-import { useTenantData } from "./hooks/useTenantData.hook";
-import { FeatureProvider } from "./context/FeatureContext";
-import AgencieEdit from "./pages/AgencieEdit";
-import AgencieAdd from "./pages/AgencieAdd";
-import { useAppConfigContext } from "./context/useAppConfig";
-import { useUserRoles } from "./hooks/useUserRoles.hook";
-import { UserRole } from "./enums/UserRole";
+import { TenantSettings } from './pages/TenantSettings';
+import { Counselors } from './pages/Counselors';
+import { Topics } from './pages/Topics';
+import { Statistic } from './pages/Statistic';
+import { UserProfile } from './pages/UserProfile';
+import { Tenants } from './pages/Tenants';
+import { Initialization } from './components/Layout/Initialization';
+import { Agencies } from './pages/Agencies';
+import { useTenantData } from './hooks/useTenantData.hook';
+import { FeatureProvider } from './context/FeatureContext';
+import { AgencyPageEdit } from './pages/AgencieEdit';
+import { AgencyAdd } from './pages/AgencieAdd';
+import { useAppConfigContext } from './context/useAppConfig';
+import { useUserRoles } from './hooks/useUserRoles.hook';
+import { UserRole } from './enums/UserRole';
 
-function App() {
-  const { settings } = useAppConfigContext();
-  const [, hasRole] = useUserRoles();
-  const { isLoading, data } = useTenantData();
-  const navigate = useNavigate();
-  const location = useLocation();
+export const App = () => {
+    const { settings } = useAppConfigContext();
+    const [, hasRole] = useUserRoles();
+    const { isLoading, data } = useTenantData();
+    const navigate = useNavigate();
+    const location = useLocation();
 
-  useEffect(() => {
-    if (
-      location.pathname === routePathNames.root ||
-      location.pathname === `${routePathNames.root}/`
-    ) {
-      const redirectPath =
-        (settings.mainTenantSubdomainForSingleDomainMultitenancy &&
-          hasRole(UserRole.TenantAdmin)) ||
-        !settings.mainTenantSubdomainForSingleDomainMultitenancy
-          ? routePathNames.themeSettings
-          : routePathNames.counselors;
-      navigate(redirectPath);
-    }
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    useEffect(() => {
+        if (location.pathname === routePathNames.root || location.pathname === `${routePathNames.root}/`) {
+            const redirectPath =
+                (settings.mainTenantSubdomainForSingleDomainMultitenancy && hasRole(UserRole.TenantAdmin)) ||
+                !settings.mainTenantSubdomainForSingleDomainMultitenancy
+                    ? routePathNames.themeSettings
+                    : routePathNames.counselors;
+            navigate(redirectPath);
+        }
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  return isLoading ? (
-    <Initialization />
-  ) : (
-    <FeatureProvider tenantData={data}>
-      <ProtectedPageLayoutWrapper>
-        <Routes>
-          {/* later <Route path="/" element={<Dashboard />} /> */}
-          <Route
-            path={routePathNames.themeSettings}
-            element={<TenantSettings />}
-          />
-          <Route path={routePathNames.counselors} element={<Counselors />} />
-          <Route path={routePathNames.agency} element={<Agencies />} />
-          <Route
-            path={`${routePathNames.agencyEdit}/*`}
-            element={<AgencieEdit />}
-          />
-          <Route
-            path={`${routePathNames.agencyAdd}/*`}
-            element={<AgencieAdd />}
-          />
-          <Route path={routePathNames.topics} element={<Topics />} />
-          <Route path={routePathNames.statistic} element={<Statistic />} />
-          <Route path={routePathNames.userProfile} element={<UserProfile />} />
-          <Route path={routePathNames.tenants} element={<Tenants />} />
-        </Routes>
-      </ProtectedPageLayoutWrapper>
-    </FeatureProvider>
-  );
-}
-
-export default App;
+    return isLoading ? (
+        <Initialization />
+    ) : (
+        <FeatureProvider tenantData={data}>
+            <ProtectedPageLayoutWrapper>
+                <Routes>
+                    {/* later <Route path="/" element={<Dashboard />} /> */}
+                    <Route path={routePathNames.themeSettings} element={<TenantSettings />} />
+                    <Route path={routePathNames.counselors} element={<Counselors />} />
+                    <Route path={routePathNames.agency} element={<Agencies />} />
+                    <Route path={`${routePathNames.agencyEdit}/*`} element={<AgencyPageEdit />} />
+                    <Route path={`${routePathNames.agencyAdd}/*`} element={<AgencyAdd />} />
+                    <Route path={routePathNames.topics} element={<Topics />} />
+                    <Route path={routePathNames.statistic} element={<Statistic />} />
+                    <Route path={routePathNames.userProfile} element={<UserProfile />} />
+                    <Route path={routePathNames.tenants} element={<Tenants />} />
+                </Routes>
+            </ProtectedPageLayoutWrapper>
+        </FeatureProvider>
+    );
+};
--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,47 +1,39 @@
-import getLocationVariables from "./utils/getLocationVariables";
+import getLocationVariables from './utils/getLocationVariables';
 
-const REACT_APP_CSRF_WHITELIST_HEADER_PROPERTY =
-  process.env.REACT_APP_CSRF_WHITELIST_HEADER_FOR_LOCAL_DEVELOPMENT;
+const REACT_APP_CSRF_WHITELIST_HEADER_PROPERTY = process.env.REACT_APP_CSRF_WHITELIST_HEADER_FOR_LOCAL_DEVELOPMENT;
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-export const CSRF_WHITELIST_HEADER: string =
-  REACT_APP_CSRF_WHITELIST_HEADER_PROPERTY;
+export const CSRF_WHITELIST_HEADER: string = REACT_APP_CSRF_WHITELIST_HEADER_PROPERTY;
 const { subdomain, origin } = getLocationVariables();
 
 let url = origin;
 
-if (process.env.REACT_APP_USE_API_URL === "true") {
-  url = `https://${process.env.REACT_APP_API_URL}`;
-} else if (origin.includes("localhost")) {
-  url = `https://${subdomain && `${subdomain}.`}${
-    process.env.REACT_APP_API_URL
-  }`;
+if (process.env.REACT_APP_USE_API_URL === 'true') {
+    url = `https://${process.env.REACT_APP_API_URL}`;
+} else if (origin.includes('localhost')) {
+    url = `https://${subdomain && `${subdomain}.`}${process.env.REACT_APP_API_URL}`;
 }
 
 export const mainURL = url;
 
 export const clusterFeatureFlags = {
-  useApiClusterSettings: true, // Feature flag to enable the cluster use the cluster settings instead of the config file
+    useApiClusterSettings: true, // Feature flag to enable the cluster use the cluster settings instead of the config file
 };
 
-export const agencyDataAgencyId = (agencyId: string) =>
-  `${mainURL}/service/agencyadmin/agencies/${agencyId}`;
+export const agencyDataAgencyId = (agencyId: string) => `${mainURL}/service/agencyadmin/agencies/${agencyId}`;
 export const agencyEndpoint = `${mainURL}/service/agencyadmin/agencies?page=1&perPage=100`;
 export const agencyEndpointBase = `${mainURL}/service/agencyadmin/agencies`;
 export const agencyEventTypes = (agencyId: string) =>
-  `${mainURL}/service/appointservice/agencies/${agencyId}/eventTypes`;
+    `${mainURL}/service/appointservice/agencies/${agencyId}/eventTypes`;
 export const agencyPostcodeRangeEndpointBase = `${mainURL}/service/agencyadmin/postcoderanges`;
 export const consultantsHasAgencyEndpoint = (agencyId: string) =>
-  `${mainURL}/service/useradmin/agencies/${agencyId}/consultants`;
+    `${mainURL}/service/useradmin/agencies/${agencyId}/consultants`;
 export const consultantsForAgencyEndpoint = (agencyId: string) =>
-  `${mainURL}/service/appointservice/agencies/${agencyId}/consultants`;
-export const consultantsForAgencyEventTypes = (
-  agencyId: string,
-  eventTypeId: number
-) =>
-  `${mainURL}/service/appointservice/agencies/${agencyId}/eventTypes/${eventTypeId}/`;
+    `${mainURL}/service/appointservice/agencies/${agencyId}/consultants`;
+export const consultantsForAgencyEventTypes = (agencyId: string, eventTypeId: number) =>
+    `${mainURL}/service/appointservice/agencies/${agencyId}/eventTypes/${eventTypeId}/`;
 export const consultantsForAgencyEventTypesNew = (agencyId: string) =>
-  `${mainURL}/service/appointservice/agencies/${agencyId}/eventTypes`;
+    `${mainURL}/service/appointservice/agencies/${agencyId}/eventTypes`;
 export const consultingTypeEndpoint = `${mainURL}/service/consultingtypes`;
 export const counselorEndpoint = `${mainURL}/service/useradmin/consultants`;
 export const eventTypeById = `${mainURL}/eventTypes/{eventTypeId}`;
@@ -58,34 +50,33 @@ export const twoFactorAuthAppEmail = `${mainURL}/service/users/2fa/email`;
 export const userDataEndpoint = `${mainURL}/service/users/data`;
 export const usersConsultantsSearchEndpoint = `${mainURL}/service/users/consultants/search`;
 export const registrationDataEndpoint = `${mainURL}/service/statistics/registration`;
-export const XHRheader = { AcceptLanguage: "de" };
+export const XHRheader = { AcceptLanguage: 'de' };
 
 /*
  * routes
  */
 const routePathNames = {
-  root: "/admin",
-  login: "/admin/login",
-  themeSettings: "/admin/theme-settings",
-  counselors: "/admin/counselor/",
-  counselorProfileEdit: "/admin/counselor/edit/",
-  counselorProfileAdd: "/admin/counselor/add/",
-  agency: "/admin/agency/",
-  agencyEdit: "/admin/agency/edit",
-  agencyEditGeneral: "/admin/agency/edit/:id/general",
-  agencyEditInitialMeeting: "/admin/agency/edit/:id/initial-meeting",
-  agencyAdd: "/admin/agency/add",
-  agencyAddGeneral: "/admin/agency/add/general",
-  topics: "/admin/topics",
-  statistic: "/admin/statistic",
-  userProfile: "/admin/profil/",
-  termsAndConditions: "/admin/agb",
-  imprint: "/impressum",
-  privacy: "/datenschutz",
-  tenants: "/admin/organisationen",
-  loginResetPasswordLink:
-    "/auth/realms/online-beratung/login-actions/reset-credentials?client_id=account",
-  appointmentServiceDevServer: "https://calcom-develop.suchtberatung.digital",
+    root: '/admin',
+    login: '/admin/login',
+    themeSettings: '/admin/theme-settings',
+    counselors: '/admin/counselor/',
+    counselorProfileEdit: '/admin/counselor/edit/',
+    counselorProfileAdd: '/admin/counselor/add/',
+    agency: '/admin/agency/',
+    agencyEdit: '/admin/agency/edit',
+    agencyEditGeneral: '/admin/agency/edit/:id/general',
+    agencyEditInitialMeeting: '/admin/agency/edit/:id/initial-meeting',
+    agencyAdd: '/admin/agency/add',
+    agencyAddGeneral: '/admin/agency/add/general',
+    topics: '/admin/topics',
+    statistic: '/admin/statistic',
+    userProfile: '/admin/profil/',
+    termsAndConditions: '/admin/agb',
+    imprint: '/impressum',
+    privacy: '/datenschutz',
+    tenants: '/admin/organisationen',
+    loginResetPasswordLink: '/auth/realms/online-beratung/login-actions/reset-credentials?client_id=account',
+    appointmentServiceDevServer: 'https://calcom-develop.suchtberatung.digital',
 };
 
 export default routePathNames;

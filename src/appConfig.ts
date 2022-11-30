e6d5eafb32c1b095d64598ed19d2f79184bbd410
Alex Benz <alex@alex-benz.de>
2022-11-30 11:48:15 +0100

    fix: changed perPage to 9999

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -26,7 +26,7 @@ export const logoutEndpoint = `${mainURL}/auth/realms/online-beratung/protocol/o
 export const tenantEndpoint = `${mainURL}/service/tenant/`;
 export const tenantPublicEndpoint = `${mainURL}/service/tenant/public/${subdomain}`;
 export const counselorEndpoint = `${mainURL}/service/useradmin/consultants`;
-export const agencyEndpoint = `${mainURL}/service/agencyadmin/agencies?page=1&perPage=10`;
+export const agencyEndpoint = `${mainURL}/service/agencyadmin/agencies?page=1&perPage=9999`;
 export const customerEndpoint = `${mainURL}/customers`;
 export const usersConsultantsSearchEndpoint = `${mainURL}/service/users/consultants/search`;
 

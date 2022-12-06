7e0c949b21677a54cbadb977f39cb3baaf6e64de
Carlos Soares <carlos.soares17@gmail.com>
2022-12-06 09:03:35 +0000

    feat: adding the form fields to simplify forms

---
+++ src/components/FormColorSelectorField/index.tsx
@@ -0,0 +1,42 @@
+import { Form } from 'antd';
+import { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+import DisabledContext from 'antd/es/config-provider/DisabledContext';
+import classNames from 'classnames';
+import ColorSelector from '../ColorSelector/ColorSelector';
+
+interface FormColorSelectorFieldProps {
+    className?: string;
+    name?: string | string[];
+    labelKey?: string;
+    required?: boolean;
+}
+
+interface FormColorSelectorProps {
+    onChange?: (value: string) => void;
+    value?: string;
+    labelKey?: string;
+}
+
+const FormColorSelectorLocal = ({ labelKey, onChange, value }: FormColorSelectorProps) => {
+    const contextDisabled = useContext(DisabledContext);
+    const { t } = useTranslation();
+
+    return (
+        <ColorSelector
+            isLoading={contextDisabled}
+            label={t(labelKey)}
+            tenantColor={value}
+            setColorValue={onChange}
+            field="primaryColor"
+        />
+    );
+};
+
+export const FormColorSelectorField = ({ className, name, labelKey, required }: FormColorSelectorFieldProps) => {
+    return (
+        <Form.Item className={classNames(className)} name={name} rules={[{ required }]}>
+            <FormColorSelectorLocal labelKey={labelKey} />
+        </Form.Item>
+    );
+};
---
+++ src/components/FormFileUploaderField/index.tsx
@@ -0,0 +1,69 @@
+import classNames from 'classnames';
+import { Form, message, Upload } from 'antd';
+import { useTranslation } from 'react-i18next';
+import { UploadFileProps } from '../../types/uploadFiles';
+import decodeHTML from '../../utils/decodeHTML';
+import getBase64 from '../../utils/getBase64';
+import styles from './styles.module.scss';
+
+interface FormFileUploaderFieldProps {
+    className?: string;
+    labelKey?: string;
+    name?: string | string[];
+    allowIcon?: boolean;
+}
+
+interface FormRichTextEditorProps {
+    onChange?: (value: string) => void;
+    value?: string;
+    allowIcon: boolean;
+}
+
+const FormFileUploaderLocal = ({ onChange, value, allowIcon }: FormRichTextEditorProps) => {
+    const { t } = useTranslation();
+
+    const beforeUpload = (file: UploadFileProps) => {
+        const isJpgOrPng =
+            file.type === 'image/jpeg' ||
+            file.type === 'image/png' ||
+            (allowIcon && (file.type === 'image/x-icon' || file.type === 'image/vnd.microsoft.icon'));
+        if (!isJpgOrPng) {
+            message.error(t('message.error.upload.filetype'));
+            return false;
+        }
+        const isLarger500kb = file.size / 1024 > 512;
+
+        if (isLarger500kb) {
+            message.error(t('message.error.upload.filesize'));
+            return false;
+        }
+
+        getBase64(file, onChange);
+        return false;
+    };
+
+    return (
+        <Upload
+            name="upload"
+            listType="picture-card"
+            className="fileUploader"
+            showUploadList={false}
+            beforeUpload={beforeUpload}
+        >
+            {value ? (
+                <img src={decodeHTML(value)} className={styles.image} alt="" />
+            ) : (
+                <div className={styles.uploadButton}>{t('btn.upload')}</div>
+            )}
+        </Upload>
+    );
+};
+
+export const FormFileUploaderField = ({ name, labelKey, className, allowIcon }: FormFileUploaderFieldProps) => {
+    const { t } = useTranslation();
+    return (
+        <Form.Item name={name} label={t(labelKey)} className={classNames(className, styles.richEditor)}>
+            <FormFileUploaderLocal allowIcon={allowIcon} />
+        </Form.Item>
+    );
+};

26988b351981a9daf655f3919f5274ce378023a9
Carlos Soares <carlos.soares17@gmail.com>
2022-12-08 12:11:15 +0000

    feat: improve users tabs

--- src/pages/users/List/index.tsx
+++ src/pages/users/List/index.tsx
@@ -1,6 +1,4 @@
-import { Tabs } from 'antd';
-import { useTranslation } from 'react-i18next';
-import { useNavigate, useParams } from 'react-router';
+import { useParams } from 'react-router';
 import { Page } from '../../../components/Page';
 import { PermissionAction } from '../../../enums/PermissionAction';
 import { Resource } from '../../../enums/Resource';
@@ -9,32 +7,25 @@ import { UsersTableData } from './components/UsersTableData';
 
 export const UsersList = () => {
     const { can } = useUserPermissions();
-    const { t } = useTranslation();
     const { typeOfUsers } = useParams();
-    const navigate = useNavigate();
-
-    const items = [
-        can(PermissionAction.Read, Resource.Consultant) && {
-            label: t('counselor.title'),
-            key: 'consultants',
-            children: <UsersTableData key={`consultants-${typeOfUsers}`} />,
-        },
-        can(PermissionAction.Read, Resource.Admin) && {
-            label: t('admins.title'),
-            key: 'admins',
-            children: <UsersTableData key={`admins-${typeOfUsers}`} />,
-        },
-    ];
 
     return (
         <Page>
-            <Page.Title titleKey="users.title" subTitleKey={`${typeOfUsers}.title.text`} />
-
-            <Tabs
-                activeKey={typeOfUsers || items[0].key}
-                items={items}
-                onTabClick={(key) => navigate(`/admin/users/${key}`)}
+            <Page.Title
+                titleKey="users.title"
+                subTitleKey={`${typeOfUsers}.title.text`}
+                tabs={[
+                    can(PermissionAction.Read, Resource.Consultant) && {
+                        to: '/admin/users/consultants',
+                        titleKey: 'counselor.title',
+                    },
+                    can(PermissionAction.Read, Resource.Admin) && {
+                        to: '/admin/users/admins',
+                        titleKey: 'admins.title',
+                    },
+                ]}
             />
+            <UsersTableData key={`admins-${typeOfUsers}`} />
         </Page>
     );
 };

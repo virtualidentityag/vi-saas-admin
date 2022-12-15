817694156f1d12e577dd782cd4cea3cc3a65639b
Carlos Soares <carlos.soares17@gmail.com>
2022-12-15 13:18:51 +0000

    feat adding the popup after the language changed

--- src/hooks/useTenantAdminDataMutation.hook.ts
+++ src/hooks/useTenantAdminDataMutation.hook.ts
@@ -1,7 +1,7 @@
 import { message } from 'antd';
 import mergeWith from 'lodash.mergewith';
 import { useTranslation } from 'react-i18next';
-import { useMutation, useQueryClient } from 'react-query';
+import { useMutation, useQueryClient, UseMutationOptions } from 'react-query';
 import { fetchData, FETCH_METHODS } from '../api/fetchData';
 import { tenantAdminEndpoint } from '../appConfig';
 import { TenantAdminData } from '../types/TenantAdminData';
@@ -28,7 +28,9 @@ const mergeData = (currentTenantData: TenantAdminData, formData) => {
     return finalData;
 };
 
-export const useTenantAdminDataMutation = () => {
+export const useTenantAdminDataMutation = (
+    options?: UseMutationOptions<Partial<TenantAdminData>, unknown, Partial<TenantAdminData>>,
+) => {
     const { t } = useTranslation();
     const queryClient = useQueryClient();
     const { data: tenantData } = useTenantData();
@@ -45,12 +47,14 @@ export const useTenantAdminDataMutation = () => {
             });
         },
         {
-            onSuccess: (_, updatedData) => {
+            ...options,
+            onSuccess: (responseData, updatedData) => {
                 queryClient.setQueryData(TENANT_ADMIN_DATA_KEY, mergeData(tenantAdminData, updatedData));
                 message.success({
                     content: t('message.success.setting.update'),
                     duration: 3,
                 });
+                options?.onSuccess?.(responseData, updatedData, null);
             },
         },
     );

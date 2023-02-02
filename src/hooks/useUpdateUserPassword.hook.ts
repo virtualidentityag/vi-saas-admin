90ae2d215a03bac8a17132886c269e18a04cc0f2
Carlos Soares <carlos.soares17@gmail.com>
2023-02-02 15:26:40 +0000

    feat: change the password data

--- src/hooks/useUpdateUserPassword.hook.ts
+++ src/hooks/useUpdateUserPassword.hook.ts
@@ -1,20 +1,16 @@
 import { useMutation, UseMutationOptions } from 'react-query';
-import { fetchData, FETCH_ERRORS, FETCH_METHODS, FETCH_SUCCESS } from '../api/fetchData';
+import { fetchData, FETCH_METHODS } from '../api/fetchData';
 import { mainURL } from '../appConfig';
 
 export const useUpdateUserPassword = (
     options?: UseMutationOptions<void, Error, { oldPassword: string; newPassword: string }, Error | Response>,
 ) => {
-    return useMutation((formData) => {
-        const bodyData = JSON.stringify({
-            ...formData,
-        });
-
+    return useMutation(({ oldPassword, newPassword }) => {
         return fetchData({
             url: `${mainURL}/service/users/password/change`,
             method: FETCH_METHODS.PUT,
-            responseHandling: [FETCH_SUCCESS.CONTENT, FETCH_ERRORS.CONFLICT_WITH_RESPONSE],
-            bodyData,
+            responseHandling: [],
+            bodyData: JSON.stringify({ newPassword, oldPassword }),
         });
     }, options);
 };

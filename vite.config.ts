1240f233ae1c9577608f2831ab740ee20fc87140
Marc Itzenthaler <itzenthaler@web-mi.de>
2023-08-16 18:19:47 +0200

    fix: docker build

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default ({ mode }) => {
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
     return defineConfig({
+        base: process.env.BASE || '/',
         plugins: [
             react(),
             viteTsconfigPaths(),

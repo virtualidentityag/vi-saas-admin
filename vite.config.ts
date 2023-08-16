89249eee628e9fd8db745534532df63412274178
Marc Itzenthaler <itzenthaler@web-mi.de>
2023-08-16 19:18:41 +0200

    fix: base path

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default ({ mode }) => {
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
     return defineConfig({
-        base: '',
+        base: process.env.BASE || '/',
         plugins: [
             react(),
             viteTsconfigPaths(),

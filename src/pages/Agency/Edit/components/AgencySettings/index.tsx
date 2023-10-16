515ec23155b7dfdf3c945546ef7623c2e5f7d9a2
Vadim Makeev <vadim.makeev@virtual-identity.com>
2023-10-16 09:06:53 +0200

    Remove required attr

--- src/pages/Agency/Edit/components/AgencySettings/index.tsx
+++ src/pages/Agency/Edit/components/AgencySettings/index.tsx
@@ -52,7 +52,6 @@ export const AgencySettings = () => {
             <SelectFormField
                 name="tenantId"
                 placeholder="tenantAdmins.form.tenant"
-                required
                 className={styles.select}
                 label="tenantAdmins.form.tenantAssignment"
             >

12761acbab6fd41ad0d2aaed75bab300a4f762bd
janrembold <janrembold@gmx.net>
2024-03-20 11:50:31 +0100

    fix: always use first consultingType as workaround

--- src/api/consultingtype/getConsultingType4Tenant.ts
+++ src/api/consultingtype/getConsultingType4Tenant.ts
@@ -1,9 +1,7 @@
 import { FETCH_ERRORS, FETCH_METHODS, fetchData } from '../fetchData';
 import { consultingTypeEndpoint } from '../../appConfig';
-import { parseUserAuthInfo } from '../../utils/parseUserAuthInfo';
 
 export default async function getConsultingType4Tenant(): Promise<string> {
-    const { tenantId } = parseUserAuthInfo();
     const consultingTypeResponse = await fetchData({
         url: `${consultingTypeEndpoint}/basic`,
         method: FETCH_METHODS.GET,
@@ -11,9 +9,6 @@ export default async function getConsultingType4Tenant(): Promise<string> {
         responseHandling: [FETCH_ERRORS.CATCH_ALL],
     });
 
-    // SAAS currently doesn't support multiple consulting types per tenant
-    if (tenantId !== undefined) {
-        return consultingTypeResponse.find((consultingType) => consultingType.tenantId === tenantId)?.id;
-    }
+    // as consulting types are not used anymore and will be removed in the future we simply pick the first entry here
     return consultingTypeResponse[0].id;
 }
